263446069f1f22db6f20bd01b903d6d01a827941
Arkadiy <alibullterrier@gmail.com>
2023-05-11 05:50:51 +0300

    return

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -15,7 +15,9 @@ export const EditableSpan = (props: PropsType) => {
         }
        
     }
-   
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setUpdateTitle(e.currentTarget.value)
+    }
 
     const addTask = () => {
         props.callBack(updateTitle)
