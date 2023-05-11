1e2c6cb608fc0a355fe9b94ce82c990b065f0eb2
Arkadiy <alibullterrier@gmail.com>
2023-05-11 05:50:08 +0300

    func add task

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -8,7 +8,15 @@ export const AddItemForm = (props: PropsType) => {
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
 
-   
+    const addTask = () => {
+        let newTitle = title.trim();
+        if (newTitle !== "") {
+            props.callBack(newTitle);
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
