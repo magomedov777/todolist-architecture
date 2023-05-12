87199ccf71a576522761f1db0b38a2f083f764dd
Arkadiy <alibullterrier@gmail.com>
2023-05-12 06:31:50 +0300

    changed

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -50,7 +50,6 @@ export function Todolist(props: PropsType) {
             {/* {props.title} */}
             <button onClick={removeTodolist}>x</button>
         </h3>
-        <AddItemForm callBack={addTaskHandler}/>
         
         <ul>
             {
