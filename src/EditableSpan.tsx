dad213dd70727e5dd5e4253d968bbba0a300be81
Arkadiy <alibullterrier@gmail.com>
2023-05-14 09:11:59 +0300

    upgrade types

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,6 +1,9 @@
 import React, { ChangeEvent, useState } from 'react';
 
-
+type PropsType = {
+    oldTitle: string
+    callBack: (updateTitle: string) => void
+}
 
 export const EditableSpan = (props: PropsType) => {
     const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
