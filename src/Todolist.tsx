a8e9dfae7bc2da06328abb4feba30de54293a9d9
Arkadiy <alibullterrier@gmail.com>
2023-05-14 09:07:55 +0300

    new solution

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -30,7 +30,9 @@ export function Todolist(props: PropsType) {
     const onActiveClickHandler = () => props.changeFilter("active", props.id);
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
 
-   
+    const addTaskHandler = (updateTitle: string) => {
+        props.addTask(updateTitle, props.id)
+    } 
     const updateTodolistTitleHandler = (updateTitle: string) => {
         props.updateTodolistTitle(props.id, updateTitle)
     }
