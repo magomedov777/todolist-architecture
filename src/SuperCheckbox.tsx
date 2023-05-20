ccb12b6088e4aacc1d09738496d48905de7025cc
Arkadiy <alibullterrier@gmail.com>
2023-05-20 08:46:09 +0300

    task

--- src/SuperCheckbox.tsx
+++ src/SuperCheckbox.tsx
@@ -7,7 +7,6 @@ type PropsType = {
 
 export const SuperCheckbox = (props: PropsType) => {
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        props.callBack(event.currentTarget.checked)
     }
   return (
     <input type="checkbox" onChange={onChangeHandler} checked={props.isDone} />
