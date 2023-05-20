99d91ac06856bb957194b14e22f6026970109e11
Arkadiy <alibullterrier@gmail.com>
2023-05-20 08:45:45 +0300

    refactor

--- src/SuperCheckbox.tsx
+++ src/SuperCheckbox.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent } from 'react'
 
 type PropsType = {
     isDone: boolean
-    callBack: () => void
+    callBack: (newIsDone: boolean) => void
 }
 
 export const SuperCheckbox = (props: PropsType) => {
