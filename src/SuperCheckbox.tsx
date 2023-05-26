8db080f4d45c6da382f9ac6d9f672f1520eed99b
Arkadiy <alibullterrier@gmail.com>
2023-05-27 00:02:36 +0300

    new func

--- src/SuperCheckbox.tsx
+++ src/SuperCheckbox.tsx
@@ -2,7 +2,6 @@ import React, { ChangeEvent } from 'react'
 
 type PropsType = {
     isDone: boolean
-    callBack: (newIsDone: boolean) => void
 }
 
 export const SuperCheckbox = (props: PropsType) => {
