9f96e5516f121f007c182bd449f69b2dbe2b28ca
Arkadiy <alibullterrier@gmail.com>
2023-06-09 06:48:19 +0300

    return

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import { v1 } from 'uuid';
 import { AddItemForm } from './AddItemForm';
 
 export type FilterValuesType = "all" | "active" | "completed";
-
+type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
 
 type TasksStateType = {
     [key: string]: Array<TaskType>
