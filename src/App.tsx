d03232df2a9a80b6d51b0e8fd57acd1f26e6e96b
Arkadiy <alibullterrier@gmail.com>
2023-07-26 06:10:11 +0300

    add new arr

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,7 @@ function App() {
     ])
 
     let [tasks, setTasks] = useState<TasksStateType>({
-        [todolistId1]: [
-            { id: v1(), title: "HTML&CSS", isDone: true },
-            { id: v1(), title: "JS", isDone: true }
-        ],
+        
         [todolistId2]: [
             { id: v1(), title: "Milk", isDone: true },
             { id: v1(), title: "React Book", isDone: true }
