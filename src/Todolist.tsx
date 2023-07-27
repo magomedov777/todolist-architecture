fd6e342b2d2148de9be51ff83a27e32a00f190a8
Arkadiy <alibullterrier@gmail.com>
2023-07-27 05:32:53 +0300

    add callback

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -49,7 +49,7 @@ export function Todolist(props: PropsType) {
 
     return <div>
         <h3>
-            <EditableSpan oldTitle={props.title} callBack={updateTodolistTitleHandler} />
+            <EditableSpan oldTitle={props.title} \ />
             {/* {props.title} */}
             <button onClick={removeTodolist}>x</button>
         </h3>
