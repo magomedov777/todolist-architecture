2a144d8480f041d47ad90e8de2e0c4e636ef15ff
Arkadiy <alibullterrier@gmail.com>
2023-07-30 07:40:05 +0300

    todo handler

--- src/App.tsx
+++ src/App.tsx
@@ -99,12 +99,7 @@ function App() {
         delete tasks[id]
     }
 
-    const addTodolistHandler = (newTitle: string) => {
-        const newTodolistId = v1()
-        const newTodolist: TodolistType = { id: newTodolistId, title: newTitle, filter: "all" }
-        setTasks({ ...tasks, [newTodolistId]: [] })
-
-    }
+    
 
     return (
         <div className="App">
