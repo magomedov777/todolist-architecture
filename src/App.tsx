7cb44e32fab23474442db59d5d32a4d67740aee5
Arkadiy <alibullterrier@gmail.com>
2023-08-03 05:17:03 +0300

    todoID

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ type TasksStateType = {
 }
 
 function App() {
-    
+    let todolistId1 = v1();
+    let todolistId2 = v1();
 
     let [todolists, setTodolists] = useState<Array<TodolistType>>([
         { id: todolistId1, title: "What to learn", filter: "all" },
