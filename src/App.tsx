67d61a86c320062e145c874fb2dee99e29a5f2de
Arkadiy <alibullterrier@gmail.com>
2023-08-07 06:14:48 +0300

    new changed

--- src/App.tsx
+++ src/App.tsx
@@ -47,26 +47,7 @@ function App() {
         setTodolists(todolists.map(el => el.id === todolistId ? {...el, title: updateTitle} : el))
     };
 
-    function removeTask(id: string, todolistId: string) {
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
-        // tasks[todolistId] = todolistTasks.filter(t => t.id != id);
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({ ...tasks });
-        setTasks({ ...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== id) })
-    }
-
-    function addTask(title: string, todolistId: string) {
-        let task = { id: v1(), title: title, isDone: false };
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-        // tasks[todolistId] = [task, ...todolistTasks];
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({ ...tasks });
-        setTasks({ ...tasks, [todolistId]: [task, ...tasks[todolistId]] })
-    }
+    
 
     function changeStatus(id: string, isDone: boolean, todolistId: string) {
         // //достанем нужный массив по todolistId:
