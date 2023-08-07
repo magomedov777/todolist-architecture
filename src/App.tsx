3c873f20cfd810fdbabc4ede692c2477ff2e4f70
Arkadiy <alibullterrier@gmail.com>
2023-08-07 06:13:41 +0300

    remove t

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,7 @@ import { AddItemForm } from './AddItemForm';
 export type FilterValuesType = "all" | "active" | "completed";
 
 
-type TodolistType = {
-    id: string
-    title: string
-    filter: FilterValuesType
-}
+
 
 type TasksStateType = {
     [key: string]: Array<TaskType>
