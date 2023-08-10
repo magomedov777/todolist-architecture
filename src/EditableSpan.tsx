a69e5af3fd49755514c963b4e24fd2124118c448
Arkadiy <alibullterrier@gmail.com>
2023-08-10 05:48:38 +0300

    rm types

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,9 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 
-type PropsType = {
-    oldTitle: string
-    callBack: (updateTitle: string) => void
-}
+
 
 export const EditableSpan = (props: PropsType) => {
     const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
