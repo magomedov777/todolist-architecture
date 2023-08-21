67b393b9514704cdd7aee55c0b14f2273510471d
Arkadiy <alibullterrier@gmail.com>
2023-08-21 06:17:05 +0300

    rm func

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -42,10 +42,7 @@ export function Todolist(props: PropsType) {
         props.updateTask(props.id, taskId, updateTitle)
     }
 
-    const onChangeHandler = (tID: string, newIsDone: boolean) => {
-        props.changeTaskStatus(tID, newIsDone, props.id);
-         // let newIsDoneValue = e.currentTarget.checked;
-    }
+    
 
     return <div>
         <h3>
