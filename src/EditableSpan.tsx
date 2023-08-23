4ab72f20a614a924f4a4aa4f13f416a3b07b949b
Arkadiy <alibullterrier@gmail.com>
2023-08-23 06:25:30 +0300

    editable span refactoring

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,16 +1,13 @@
 import React, { ChangeEvent, useState } from 'react';
 
-type PropsType = {
-    oldTitle: string
-    callBack: (updateTitle: string) => void
-}
+
 
 export const EditableSpan = (props: PropsType) => {
     const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
     const [edit, setEdit] = useState(false)
     const onDoubleClickHandler = () => {
         setEdit(!edit)
-        if(edit){
+        if (edit) {
             addTask()
         }
     }
@@ -27,7 +24,7 @@ export const EditableSpan = (props: PropsType) => {
     }
     return (
         edit
-            ? <input value={updateTitle} onBlur={onDoubleClickHandler} onChange={onChangeHandler} type="text" autoFocus/>
+            ? <input value={updateTitle} onBlur={onDoubleClickHandler} onChange={onChangeHandler} type="text" autoFocus />
             : <span onDoubleClick={onDoubleClickHandler}>{props.oldTitle}</span>
     )
 }
