650eca003f0f4397575afcbe42b43979a5e91e43
Arkadiy <alibullterrier@gmail.com>
2023-08-23 06:29:24 +0300

    create useStates

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -3,7 +3,8 @@ import React, { ChangeEvent, useState } from 'react';
 
 
 export const EditableSpan = (props: PropsType) => {
-
+    const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
+    const [edit, setEdit] = useState(false)
     const onDoubleClickHandler = () => {
         setEdit(!edit)
         if (edit) {
