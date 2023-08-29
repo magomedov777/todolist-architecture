de6cb66489895525d728d1a50add5cbaeed8140d
Arkadiy <alibullterrier@gmail.com>
2023-08-29 06:36:13 +0300

    refactor: removeTask function

--- src/App.tsx
+++ src/App.tsx
@@ -45,13 +45,13 @@ function App() {
     };
 
     function removeTask(id: string, todolistId: string) {
+        setTasks({ ...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== id) })
         // //достанем нужный массив по todolistId:
         // let todolistTasks = tasks[todolistId];
         // // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
         // tasks[todolistId] = todolistTasks.filter(t => t.id != id);
         // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
         // setTasks({ ...tasks });
-        setTasks({ ...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== id) })
     }
 
     function addTask(title: string, todolistId: string) {
