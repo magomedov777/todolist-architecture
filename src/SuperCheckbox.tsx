b4da1084aafee4921bcaf40bdfa11e509b14dad0
Arkadiy <alibullterrier@gmail.com>
2023-09-03 11:54:57 +0300

    remove for test 2

--- src/SuperCheckbox.tsx
+++ src/SuperCheckbox.tsx
@@ -1,9 +1,6 @@
 import React, { ChangeEvent } from 'react'
 
-type PropsType = {
-    isDone: boolean
-    callBack: (newIsDone: boolean) => void
-}
+
 
 export const SuperCheckbox = (props: PropsType) => {
 
