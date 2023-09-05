280271f50807d107798dc86d70ba7011502730ad
Arkadiy <alibullterrier@gmail.com>
2023-09-05 06:56:41 +0300

    titile updated

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -32,9 +32,7 @@ export function Todolist(props: PropsType) {
 
 
 
-    const updateTodolistTitleHandler = (updateTitle: string) => {
-        props.updateTodolistTitle(props.id, updateTitle)
-    }
+
 
     const updateTaskHandler = (taskId: string, updateTitle: string) => {
         props.updateTask(props.id, taskId, updateTitle)
