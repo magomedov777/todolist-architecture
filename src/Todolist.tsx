9bfc2a9909b2734364f240e6a7398663de9fbf17
Arkadiy <alibullterrier@gmail.com>
2023-09-05 06:57:05 +0300

    updated tasks

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -34,9 +34,7 @@ export function Todolist(props: PropsType) {
 
 
 
-    const updateTaskHandler = (taskId: string, updateTitle: string) => {
-        props.updateTask(props.id, taskId, updateTitle)
-    }
+
 
     const onChangeHandler = (tID: string, newIsDone: boolean) => {
         props.changeTaskStatus(tID, newIsDone, props.id);
