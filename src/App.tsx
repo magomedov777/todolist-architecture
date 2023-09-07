162ad046731724e421e9c2fc431f131b5a84d0e0
Arkadiy <alibullterrier@gmail.com>
2023-09-07 06:56:07 +0300

    fix: create id, UUID

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,8 @@ import { AddItemForm } from './AddItemForm';
 
 
 function App() {
-
+    let todolistId1 = v1();
+    let todolistId2 = v1();
 
     let [todolists, setTodolists] = useState<Array<TodolistType>>([
         { id: todolistId1, title: "What to learn", filter: "all" },
