209b0c8c0d531427f032213e98a40a820b71a2db
Arkadiy <alibullterrier@gmail.com>
2023-09-13 06:06:14 +0300

    handler new

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -39,7 +39,9 @@ export function Todolist(props: PropsType) {
         props.updateTodolistTitle(props.id, updateTitle)
     }
 
-
+    const updateTaskHandler = (taskId: string, updateTitle: string) => {
+        props.updateTask(props.id, taskId, updateTitle)
+    }
 
 
 
