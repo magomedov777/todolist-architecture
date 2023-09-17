e0a2ec5bd4cc05a8c1b429524bbde39a9403f05e
Arkadiy <alibullterrier@gmail.com>
2023-09-17 08:10:07 +0300

    add types, todo types & task state types

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import { AddItemForm } from './AddItemForm';
 
 export type FilterValuesType = "all" | "active" | "completed";
 
+type TodolistType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
 
+type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
 
 function App() {
     let todolistId1 = v1();
