8d4a3008b235f435768165631363b75d622cffc8
Arkadiy <alibullterrier@gmail.com>
2023-09-20 06:08:57 +0300

    on double click handler

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,23 +1,14 @@
 import React, { ChangeEvent, useState } from 'react';
 
-
-
 type PropsType = {
     oldTitle: string
     callBack: (updateTitle: string) => void
 }
 
-
-
 export const EditableSpan = (props: PropsType) => {
     const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
     const [edit, setEdit] = useState(false)
-    const onDoubleClickHandler = () => {
-        setEdit(!edit)
-        if (edit) {
-            addTask()
-        }
-    }
+
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setUpdateTitle(e.currentTarget.value)
     }
