6d9bf83f24ed1e7658e4628fff69cddaf60e48f1
Arkadiy <alibullterrier@gmail.com>
2023-09-22 06:11:39 +0300

    create: button filters

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -73,7 +73,12 @@ export function Todolist(props: PropsType) {
             }
         </ul>
         <div>
-
+            <button className={props.filter === 'all' ? "active-filter" : ""}
+                onClick={onAllClickHandler}>All
+            </button>
+            <button className={props.filter === 'active' ? "active-filter" : ""}
+                onClick={onActiveClickHandler}>Active
+            </button>
 
         </div>
     </div>
