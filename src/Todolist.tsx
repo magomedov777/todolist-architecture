48b988f13e0c916e14d82af5fbc03661a896f07a
Arkadiy <alibullterrier@gmail.com>
2023-09-22 06:14:02 +0300

    comm newIsDone value

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -47,6 +47,7 @@ export function Todolist(props: PropsType) {
 
     const onChangeHandler = (tID: string, newIsDone: boolean) => {
         props.changeTaskStatus(tID, newIsDone, props.id);
+        // let newIsDoneValue = e.currentTarget.checked;
     }
 
     return <div>
