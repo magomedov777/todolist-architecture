c460320260c4ae723912347de7b5c7d431d8c91d
Arkadiy <alibullterrier@gmail.com>
2023-09-24 11:10:25 +0300

    fix: rewrite tipization

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ function App() {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
-    let [todolists, setTodolists] = useState<Array<TodolistType>>([
+    let [todolists, setTodolists] = useState([
         { id: todolistId1, title: "What to learn", filter: "all" },
         { id: todolistId2, title: "What to buy", filter: "all" }
     ])
 
-    let [tasks, setTasks] = useState<TasksStateType>({
+    let [tasks, setTasks] = useState({
         [todolistId1]: [
             { id: v1(), title: "HTML&CSS", isDone: true },
             { id: v1(), title: "JS", isDone: true }
