e07aa811e4e1a492da97289949884ceac11359ab
Arkadiy <alibullterrier@gmail.com>
2023-10-16 06:18:14 +0300

    task type

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -14,7 +14,7 @@ export type TaskType = {
 type PropsType = {
     id: string
     title: string
-    tasks: Array<TaskType>
+    tasks: TaskType[]
     removeTask: (taskId: string, todolistId: string) => void
     changeFilter: (value: FilterValuesType, todolistId: string) => void
     addTask: (title: string, todolistId: string) => void
